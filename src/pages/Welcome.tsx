612b5c04f38081191d53195e4c3c44e21c858a7f
Futigami <erick.ertuito@hotmail.com>
2021-04-21 14:05:42 -0300

    Refactoring

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,69 @@
+import React, {useState} from 'react';
+import { Dimensions, SafeAreaView, Text, Image, TouchableOpacity, TouchableOpacityBase, StyleSheet } from 'react-native';
+
+import wateringImg from '../assets/watering.png';
+import colors from '../styles/colors';
+
+export function Welcome(){
+
+    return(
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>
+                Gerencie {'\n'}
+                suas plantas {'\n'}
+                de forma fácil
+            </Text>
+
+                <Image source={wateringImg} style={styles.image} resizeMode="contain"/>
+
+            <Text style={styles.subtitle}>
+                Não esqueça mais de regar suas plantas. 
+                Nós cuidamos de lembrar você sempre que precisar.
+            </Text>
+
+            <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+                <Text style={styles.buttonText}>
+                    >
+                </Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    }, 
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading
+    },
+    subtitle: {
+        textAlign: 'center',
+        fontSize: 18,
+        paddingHorizontal: 20,
+        color: colors.heading
+    },
+    image:{
+        height: Dimensions.get('window').width * 0.7
+    },
+    button: {
+        backgroundColor:colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56,
+        paddingHorizontal: 10
+    },
+    buttonText:{
+        color: colors.white,
+        fontSize: 24
+    }
+
+});
